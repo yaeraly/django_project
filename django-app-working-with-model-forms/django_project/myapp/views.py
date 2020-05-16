c6c5db2c86103e3c907307d658c934b6ffa51a48
yaeraly <ya.eraly@gmail.com>
2020-05-16 23:13:44 +0300

    Django App-CRUD

--- django-app-working-with-model-forms/django_project/myapp/views.py
+++ django-app-working-with-model-forms/django_project/myapp/views.py
@@ -14,20 +14,21 @@ class HomeListView(generic.ListView):
 class CreateReporterView(generic.CreateView):
     model = Reporter
     fields = ['first_name']
-    template_name_suffix = '_create_form'
     template_name = 'myapp/create.html'
+    template_name_suffix = '_create_form'
     success_url = reverse_lazy('home')
 
 
 class UpdateReporterView(generic.UpdateView):
     model = Reporter
     fields = ['first_name']
-    template_name_suffix = '_update_form'
     template_name = 'myapp/update.html'
+    template_name_suffix = '_update_form'
     success_url = reverse_lazy('home')
 
 
 class DeleteReporterView(generic.DeleteView):
     model =  Reporter
     template_name = 'myapp/delete.html'
+    template_name_suffix = '_check_delete'
     success_url = reverse_lazy('home')
