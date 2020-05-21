fecec439e3f5648c26dc2a16ce46d6ba71e20a7c
yaeraly <ya.eraly@gmail.com>
2020-05-21 17:27:47 +0300

    Includes Templates and Database

--- django-app-working-with-modal-forms/django_project/modal/models.py
+++ django-app-working-with-modal-forms/django_project/modal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+	first_name 	= models.CharField(max_length=30)
+	last_name 	= models.CharField(max_length=30)
+	full_name 	= models.CharField(max_length=60, blank=True)
+	email		= models.EmailField(max_length=30)
+
+	def __str__(self):
+		return self.first_name
+
+	def save(self, *args, **kwargs):
+		self.full_name = '{0} {1}'.format(self.first_name, self.last_name)
+		super(Employee, self).save(*args, **kwargs)
