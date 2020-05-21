ccd9a7e4b244a1760a13d8fa8ab8bd6f3477c54e
yaeraly <ya.eraly@gmail.com>
2020-05-21 13:59:39 +0300

    Deleted App

--- django-app-working-with-modal-forms/django_project/django_project/settings.py
+++ django-app-working-with-modal-forms/django_project/django_project/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'modal.apps.ModalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django-app-working-with-modal-forms/django_project/modal/models.py
+++ django-app-working-with-modal-forms/django_project/modal/models.py
@@ -1,15 +1,1 @@
-from django.db import models
 
-class Post(models.Model):
-	title = models.CharField(max_length=30, null=True, blank=True)
-
-	def __str__(self):
-		return self.title
-
-
-class Comment(models.Model):
-	content = models.CharField(max_length=300, null=True, blank=True)
-	post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-	def __str__(self):
-		return self.content
