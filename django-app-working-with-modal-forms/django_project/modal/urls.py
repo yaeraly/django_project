e661e45b2c88caf9730b3e6e70198b953915a377
yaeraly <ya.eraly@gmail.com>
2020-05-21 21:08:44 +0300

    Ready for Modal Pop-Up Form

--- django-app-working-with-modal-forms/django_project/modal/urls.py
+++ django-app-working-with-modal-forms/django_project/modal/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 app_name = 'modal'
 urlpatterns = [
-	path('', views.home, name='home'),
+	path('', views.HomeListView.as_view(), name='home'),
+	path('create/', views.CreateEmployeeView.as_view(), name='create'),
+	path('update/<int:pk>/', views.UpdateEmployeeView.as_view(), name='update'),
+	path('delete/<int:pk>/', views.DeleteEmployeeView.as_view(), name='delete'),
+
 ]
--- django-app-working-with-modal-forms/django_project/modal/views.py
+++ django-app-working-with-modal-forms/django_project/modal/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
 
-def home(request):
-	return render(request, 'modal/home.html')
+from .models import Employee
+
+
+class HomeListView(generic.ListView):
+	model = Employee
+	template_name = 'modal/home.html'
+
+
+class CreateEmployeeView(generic.CreateView):
+	model = Employee
+	fields = ['first_name', 'last_name', 'email']
+	template_name = 'modal/create.html'
+	success_url = reverse_lazy('modal:home')
+
+
+class DeleteEmployeeView(generic.DeleteView):
+	model = Employee
+	template_name = 'modal/delete.html'
+	success_url = reverse_lazy('modal:home')
+
+
+class UpdateEmployeeView(generic.UpdateView):
+	model = Employee
+	fields = ['first_name', 'last_name', 'email']
+	template_name = 'modal/update.html'
+	success_url = reverse_lazy('modal:home')
