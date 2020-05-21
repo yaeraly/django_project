921c2cce23fd4ca8b8dcfab598b509ed66744738
yaeraly <ya.eraly@gmail.com>
2020-05-21 14:18:08 +0300

    Modal is included to Installed Apps

--- django-app-working-with-modal-forms/django_project/django_project/settings.py
+++ django-app-working-with-modal-forms/django_project/django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'modal.apps.ModalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
