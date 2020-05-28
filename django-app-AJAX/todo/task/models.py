9cbe2c246280068a2214b1b499fd368db879bc26
yaeraly <ya.eraly@gmail.com>
2020-05-28 18:06:08 +0300

    Django AJAX

---
+++ django-app-AJAX/todo/task/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Task(models.Model):
+	title		= models.CharField(max_length = 255)
+	date 		= models.DateTimeField(auto_now_add = True)
+	completed	= models.BooleanField(default = False)
+
+	def __str__(self):
+		return self.title
+
+
+	class Meta:
+		ordering = ['completed', 'date']
---
+++ django-app-AJAX/todo/task/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+	path('', views.TaskList.as_view(), name='task_list_url'),
+	path('<str:id>/completed/', views.TaskComplete.as_view(), name='task_complete_url'),
+	path('<str:id>/delete/', views.TaskDelete.as_view(), name='task_delete_url'),
+]
