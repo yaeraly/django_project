d029db597451c3637810851e61901ec438c67a0d
yaeraly <ya.eraly@gmail.com>
2020-05-29 21:53:18 +0300

    Bootsrap Added

--- django-app-AJAX/todo/task/views.py
+++ django-app-AJAX/todo/task/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from django.views.generic import View 
 
 
+from .models import Task 
+from .forms import TaskForm
+
+
 
 class TaskList(View):
 	def get(self, request):
-		return render(request, 'task/task_list.html')
+		context = {}
+		
+		form = TaskForm()
+		tasks = Task.objects.all()
+
+		context['form'] = form
+		context['tasks'] = tasks 
+		return render(request, 'task/task_list.html', context)
