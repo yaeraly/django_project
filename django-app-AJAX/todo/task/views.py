bcefe0bc312aa04f9f14d2a3c65a8406db4fe491
yaeraly <ya.eraly@gmail.com>
2020-05-29 22:14:06 +0300

    TaskList View is Created

--- django-app-AJAX/todo/task/views.py
+++ django-app-AJAX/todo/task/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View 
 
 
@@ -17,3 +17,13 @@ class TaskList(View):
 		context['form'] = form
 		context['tasks'] = tasks 
 		return render(request, 'task/task_list.html', context)
+
+
+	def post(self, request):
+		form = TaskForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			return redirect('task_list_url')
+		else:
+			return redirect('task_list_url')
