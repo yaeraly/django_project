e4a8abeaedea7213881df79d1bb23362d28b7e28
yaeraly <ya.eraly@gmail.com>
2020-05-30 05:20:28 +0300

    Ready for Ajax

--- django-app-AJAX/todo/task/views.py
+++ django-app-AJAX/todo/task/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+
+
+from .models import Task 
+from .forms import TaskForm
 
 
 class TaskList(View):
 	def get(self, request):
-		return render(request, 'task/task_list.html')
+		context = {}
+		form = TaskForm()
+		tasks = Task.objects.all()
+
+		context['form'] = form
+		context['tasks'] = tasks
+		return render(request, 'task/task_list.html', context)
+
+
+	def post(self, request):
+		form = TaskForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+
+			return redirect('task_list_url')
+		else:
+			return redirect('task_list_url')
