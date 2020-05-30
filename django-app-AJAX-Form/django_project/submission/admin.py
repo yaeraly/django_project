7d3140531dfc27916d5e6e30830590a46a30a96e
yaeraly <ya.eraly@gmail.com>
2020-05-30 18:27:25 +0300

    Model

--- django-app-AJAX-Form/django_project/submission/admin.py
+++ django-app-AJAX-Form/django_project/submission/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person
+
+
+class PersonAdmin(admin.ModelAdmin):
+	pass
+
+
+admin.site.register(Person, PersonAdmin)
--- django-app-AJAX-Form/django_project/submission/models.py
+++ django-app-AJAX-Form/django_project/submission/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+	first_name		= models.CharField(max_length=50)
+	last_name 		= models.CharField(max_length=50)
+	full_name		= models.CharField(max_length=101, blank=True)
+
+	def __str__(self):
+		return self.first_name
+
+
+	def save(self, *args, **kwargs):
+		self.full_name = self.first_name + ' ' + self.last_name
+
+		super(Person, self).save(*args, **kwargs)
