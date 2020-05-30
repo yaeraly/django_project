b1684bf64363976cf3dddc1c7f9d23441c896dad
yaeraly <ya.eraly@gmail.com>
2020-05-30 20:57:15 +0300

    Ready for Ajax

--- django-app-AJAX-Form/django_project/submission/forms.py
+++ django-app-AJAX-Form/django_project/submission/forms.py
@@ -1,3 +1,23 @@
 from django import forms
 
 from .models import Person
+
+
+class PersonForm(forms.ModelForm):
+	first_name = forms.CharField(
+		widget=forms.TextInput(attrs={
+			'class': 'form-control', 
+			'type': 'text',
+			'placeholder': 'First Name'
+		})
+	)
+	last_name = forms.CharField(
+		widget=forms.TextInput(attrs={
+			'class': 'form-control', 
+			'type': 'text',
+			'placeholder': 'Last Name'
+		})
+	)
+	class Meta:
+		model = Person 
+		fields = ['first_name', 'last_name']
--- django-app-AJAX-Form/django_project/submission/views.py
+++ django-app-AJAX-Form/django_project/submission/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+
+
+from .models import Person
+from .forms import PersonForm
 
 
 class FormView(View):
 	def get(self, request):
-		return render(request, 'submission/submission_form.html')
+		context = {}
+		form = PersonForm()
+		people = Person.objects.all()
+		context['people'] = people
+		context['form'] = form
+		return render(request, 'submission/submission_form.html', context)
+
+	def post(self, request):
+		form = PersonForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			return redirect('form')
+		else:
+			return redirect('form')
